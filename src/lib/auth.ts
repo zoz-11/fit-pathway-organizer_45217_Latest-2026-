f775a6efc7697591e564d7113de14bae92b2c623
zoz-11 <izaa2024a@icloud.com>
2026-04-16 21:05:58 +0300

    Align auth password strength helper to minimum 6 characters

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -145,24 +145,8 @@ export const validateSession = async (
 export const validatePasswordStrength = (password: string): { isValid: boolean; errors: string[] } => {
   const errors: string[] = [];
   
-  if (password.length < 8) {
-    errors.push('Password must be at least 8 characters long');
-  }
-  
-  if (!/[A-Z]/.test(password)) {
-    errors.push('Password must contain at least one uppercase letter');
-  }
-  
-  if (!/[a-z]/.test(password)) {
-    errors.push('Password must contain at least one lowercase letter');
-  }
-  
-  if (!/\d/.test(password)) {
-    errors.push('Password must contain at least one number');
-  }
-  
-  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-    errors.push('Password must contain at least one special character');
+  if (password.length < 6) {
+    errors.push('Password must be at least 6 characters long');
   }
   
   return {
